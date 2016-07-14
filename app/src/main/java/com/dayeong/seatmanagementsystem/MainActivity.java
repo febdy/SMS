0465d533c96f5bbcd1d7116022a6c88fcd83a91e
ldy <feb29dy@gmail.com>
2016-07-14 23:59:13 +0900

    can get current location on the map with GPS

--- app/src/main/java/com/dayeong/seatmanagementsystem/MainActivity.java
+++ app/src/main/java/com/dayeong/seatmanagementsystem/MainActivity.java
@@ -2,7 +2,6 @@ package com.dayeong.seatmanagementsystem;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -10,6 +9,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
+import net.daum.mf.map.api.MapPoint;
 import net.daum.mf.map.api.MapView;
 
 public class MainActivity extends AppCompatActivity {
@@ -21,21 +21,35 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        final MapView mapView = new MapView(this);
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab_gps);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                getGps(mapView);
             }
         });
 
-        MapView mapView = new MapView(this);
         mapView.setDaumMapApiKey("40835261670c49406a6124ee35c9cba8");
-
         ViewGroup mapViewContainer = (ViewGroup) findViewById(R.id.map_view);
+        getGps(mapView);
         mapViewContainer.addView(mapView);
 
+    }
+
+    public void getGps(MapView mapView) {
+        GpsInfo gps = new GpsInfo(MainActivity.this);
+
+        if (gps.isGetLocation()) {
+            double latitude = gps.getLatitude();
+            double longitude = gps.getLongitude();
+
+            mapView.setMapCenterPointAndZoomLevel(MapPoint.mapPointWithGeoCoord(latitude, longitude), 0, true);
+
+        } else {
+            gps.showSettingsAlert();
+        }
     }
 
     @Override
