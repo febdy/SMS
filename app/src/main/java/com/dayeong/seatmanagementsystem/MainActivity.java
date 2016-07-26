565914b1b091814058dfcc0ff00ce308096c64ec
ldy <feb29dy@gmail.com>
2016-07-26 22:41:19 +0900

    add SearchView as AutoCompleteTextView

--- app/src/main/java/com/dayeong/seatmanagementsystem/MainActivity.java
+++ app/src/main/java/com/dayeong/seatmanagementsystem/MainActivity.java
@@ -3,12 +3,16 @@ package com.dayeong.seatmanagementsystem;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
 
 import net.daum.mf.map.api.MapPOIItem;
 import net.daum.mf.map.api.MapPoint;
@@ -16,12 +20,15 @@ import net.daum.mf.map.api.MapView;
 
 public class MainActivity extends AppCompatActivity implements MapView.POIItemEventListener {
 
+    String[] itemArrayList = {"test1", "test2", "test3", "test4", "test5"};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
 
         final MapView mapView = new MapView(this);
 
@@ -88,18 +95,33 @@ public class MainActivity extends AppCompatActivity implements MapView.POIItemEv
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        MenuItem searchItem = menu.findItem(R.id.action_search);
+        final ArrayAdapterSearchView searchView = (ArrayAdapterSearchView) MenuItemCompat.getActionView(searchItem);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, itemArrayList);
+        searchView.setAdapter(adapter);
+
+        searchView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String searchString = (String) parent.getItemAtPosition(position);
+                searchView.setText(parent.getItemAtPosition(position).toString()); // TODO: 커서위치 뒤로
+                Toast.makeText(MainActivity.this, "you clicked " + searchString, Toast.LENGTH_SHORT).show();
+            }
+        });
+
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            return true;
+        } else if (id == R.id.action_search) {
             return true;
         }
 
